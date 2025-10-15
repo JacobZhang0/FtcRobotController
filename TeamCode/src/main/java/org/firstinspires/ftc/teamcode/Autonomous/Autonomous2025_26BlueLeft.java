6affa8aa01c3ef850fc56c71f119311f7526df30
JacobZhang0 <jacob.zhang8@icloud.com>
2025-10-14 20:31:24 -0400

    Add Apriltag Implementation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Autonomous2025_26BlueLeft.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Autonomous2025_26BlueLeft.java
@@ -4,26 +4,86 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.ExposureControl;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.GainControl;
 import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagGameDatabase;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class Autonomous2025_26BlueLeft extends LinearOpMode {
 
     private static final boolean USE_WEBCAM = true; // true for webcam, false for phone camera
+    private static final int DESIRED_TAG_ID = -1;     // Choose the tag you want to approach or set to -1
     private AprilTagProcessor aprilTag;
     private VisionPortal visionPortal;
     private WebcamName webcam1;
+    private AprilTagDetection desiredTag = null;     // Used to hold the data for a detected AprilTag
+    private int detectedTagId;
+
+
 
     @Override
     public void runOpMode() {
 
+        boolean targetFound = false; // Set to true when an AprilTag target is detected
         webcam1 = hardwareMap.get(WebcamName.class, "Webcam1");
 
         initAprilTag();
 
+        if (USE_WEBCAM)
+            setManualExposure(6, 250);  // Use low exposure time to reduce motion blur
+
         waitForStart();
         if (isStopRequested()) return;
+
+        // Move to april tag
+
+        if (opModeIsActive()) {
+
+
+            targetFound = false;
+            desiredTag = null;
+            while (!targetFound) {
+                // Step through the list of detected tags and look for a matching tag
+                List<AprilTagDetection> currentDetections = aprilTag.getDetections();
+                for (AprilTagDetection detection : currentDetections) {
+                    // Look to see if we have size info on this tag.
+                    if (detection.metadata != null) {
+                        //  Check to see if we want to track towards this tag.
+                        if ((DESIRED_TAG_ID < 0) || (detection.id == DESIRED_TAG_ID)) {
+                            // Yes, we want to use this tag.
+                            targetFound = true;
+                            desiredTag = detection;
+                            detectedTagId = detection.id;
+                            break;  // don't look any further.
+                        } else {
+                            // This tag is in the library, but we do not want to track it right now.
+                            telemetry.addData("Skipping", "Tag ID %d is not desired", detection.id);
+                        }
+                    } else {
+                        // This tag is NOT in the library, so we don't have enough information to track to it.
+                        telemetry.addData("Unknown", "Tag ID %d is not in TagLibrary", detection.id);
+                    }
+                }
+            }
+
+            if (detectedTagId == 21) {
+
+            }
+            if (detectedTagId == 22) {
+
+            }
+            if (detectedTagId == 23) {
+
+            }
+
+
+
+        }
 
     }
 
@@ -80,5 +140,39 @@ public class Autonomous2025_26BlueLeft extends LinearOpMode {
         visionPortal = builder.build();
 
     }   // end method initAprilTag()
+
+    private void setManualExposure(int exposureMS, int gain) {
+        // Wait for the camera to be open, then use the controls
+
+        if (visionPortal == null) {
+            return;
+        }
+
+        // Make sure camera is streaming before we try to set the exposure controls
+        if (visionPortal.getCameraState() != VisionPortal.CameraState.STREAMING) {
+            telemetry.addData("Camera", "Waiting");
+            telemetry.update();
+            while (!isStopRequested() && (visionPortal.getCameraState() != VisionPortal.CameraState.STREAMING)) {
+                sleep(20);
+            }
+            telemetry.addData("Camera", "Ready");
+            telemetry.update();
+        }
+
+        // Set camera controls unless we are stopping.
+        if (!isStopRequested())
+        {
+            ExposureControl exposureControl = visionPortal.getCameraControl(ExposureControl.class);
+            if (exposureControl.getMode() != ExposureControl.Mode.Manual) {
+                exposureControl.setMode(ExposureControl.Mode.Manual);
+                sleep(50);
+            }
+            exposureControl.setExposure((long)exposureMS, TimeUnit.MILLISECONDS);
+            sleep(20);
+            GainControl gainControl = visionPortal.getCameraControl(GainControl.class);
+            gainControl.setGain(gain);
+            sleep(20);
+        }
+    }
 
 }
